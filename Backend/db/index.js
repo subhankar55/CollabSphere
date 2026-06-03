eb7f74b778aab0cc5fe97879b7ab0cfc3264344e
Subhankar <maitisubhankar55@gmail.com>
2026-06-03 10:39:17 +0530

    db name is moved to .env file

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 
 export async function connectDB() {
 
   try{
 
-      await mongoose.connect('mongodb://127.0.0.1:27017/Collabsphere',{
+      await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`,{
             serverSelectionTimeoutMS: 5000 
         });
       console.log("Connected to MongoDB");
