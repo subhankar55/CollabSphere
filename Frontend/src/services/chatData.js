d97c9abc4454edbc91617e2ff674736c6b328284
Subhankar <maitisubhankar55@gmail.com>
2026-07-02 23:31:22 +0530

    Update chatData.js

--- Frontend/src/services/chatData.js
+++ Frontend/src/services/chatData.js
@@ -17,6 +17,8 @@ export const createChat = async (workspaceid,message) => {
 }
 
 
+
+
 export const allChats = async (workspaceid) => {
     try {
         const res = await axios.get(`http://${import.meta.env.VITE_SERVER}/collabsphere/api/v1/chat/allChats/${workspaceid}`,
